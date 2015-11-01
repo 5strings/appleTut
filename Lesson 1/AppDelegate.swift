4e54db37256ee93d2e333a6ccaee48e89d124333
Jake <test@gmail.com>
2015-11-01 11:02:51 +1100

    Added a comment

--- Lesson 1/AppDelegate.swift
+++ Lesson 1/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+    // What dosen't matter 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         return true
